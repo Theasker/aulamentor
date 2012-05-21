75fedd96ea8827fe31a2f025d7e411bf409ba51b
Theasker <>
2012-05-21 15:59:10 +0200

    prueba

---
+++ Unidad5/encuesta.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+   <meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
+</head>
+<body>
+  <center>
+  <table width='500' border='1'>
+    <tr>
+      <th><h2>Encuesta</h2></th>
+    </tr>
+    <tr>
+      <td>
+        <h3>¿Cuantas veces entras en Internet?</h3>
+        <form method="POST" action="encuesta.php">
+          <input type="radio" name="encuesta" value="male" /> Male<br />
+          <input type="radio" name="encuesta" value="female" /> Female
+          
+        </form>
+      </td>
+    </tr>
+  </table>
+</body>
+</html>
+
+<?php
+
+?>
