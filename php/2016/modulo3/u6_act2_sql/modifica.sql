255fc5b03c4cb9f764a89aa4eee2a9c9ae4ee679
Theasker <theasker@gmail.com>
2016-10-05 20:20:43 +0200

    u6_act2_sql

--- php/2016/modulo3/u6_act2_sql/modifica.sql
+++ php/2016/modulo3/u6_act2_sql/modifica.sql
@@ -12,15 +12,27 @@ UPDATE compradores SET poblacion = 'Cuenca' WHERE nombre = 'Ray';
 UPDATE compras SET id_comprador = 3 WHERE compras.registro = 7;
 
 # 3.  Volver a mostrar todos los campos de todos los registros de las tres tablas.
-
+SELECT * FROM libros
+UNION ALL
+SELECT *, NULL, NULL FROM compradores
+UNION ALL
+SELECT *, NULL, NULL, NULL, NULL, NULL, NULL FROM compras;
 
 # 4.  Mostrar la información introducida en las tres tablas de la forma siguiente:
+
 # Mostrar el número de registro, el título, el país y la fecha de entrada en la librería 
 # de aquellos libros que se hayan adquirido en la librería después de 1990. 
-
+SELECT registro, titulo, pais, fecha_entrada_libreria
+FROM libros
+WHERE DATE(fecha_entrada_libreria) > '1990-01-01';
 
 # Mostrar el título del libro, el nombre, la población y el importe acumulado de los compradores mayores de 30 años.
-
+SELECT libros.titulo, compradores.nombre, compradores.poblacion
+FROM libros
+INNER JOIN compras
+ON libros.registro = compras.id_libro
+INNER JOIN compradores
+ON compras.id_comprador = compradores.registro;
 
 # Hallar la media del precio de los libros.
 
