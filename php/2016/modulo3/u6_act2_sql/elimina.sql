54d047c4a370a51c3c2d4cf7d7d0bb378816ea71
http <theasker@gmail.com>
2016-10-10 15:29:23 +0200

    u6_act2_sql --\> elimina.sql terminado \(actividad terminada\)

--- php/2016/modulo3/u6_act2_sql/elimina.sql
+++ php/2016/modulo3/u6_act2_sql/elimina.sql
@@ -33,18 +33,38 @@ ON libros.registro = compras.id_libro
 GROUP BY compras.id_libro) AS vendidos);
 
 # 4. Mostrar los campos titulo, escritor, pais, soporte y fecha de entrada en la librería de los libros no eliminados.
-
+SELECT titulo, escritor, pais, soporte, fecha_entrada_libreria
+FROM libros; 
 
 # 5. Mostrar los campos nombre, domicilio y población de todos los compradores.
-
+SELECT nombre, domicilio, poblacion
+FROM compradores;
 
 # 6. Mostrar los campos nombre, domicilio y población de los clientes que hayan comprado al menos 1 libro.
-
+SELECT compradores.nombre, compradores.domicilio, compradores.poblacion
+FROM compradores
+INNER JOIN compras
+ON compradores.registro = compras.id_comprador
+GROUP BY compradores.nombre;
 
 # 7. Borrar los compradores que no hayan comprado nunca ningún libro.
 
+# Hago que los compradores con id 7 y 5 no hayan comprado ningún libro.
+UPDATE compras SET id_comprador = 3 WHERE id_comprador = 7;
+UPDATE compras SET id_comprador = 3 WHERE id_comprador = 5;
+
+# Ahora borro los registros
+DELETE FROM compradores WHERE compradores.registro NOT IN
+(SELECT * FROM 
+
+(SELECT id_comprador
+FROM compras
+INNER JOIN compradores
+ON compras.id_comprador = compradores.registro
+GROUP BY id_comprador) AS sin_comprar);
 
 # 8. Mostrar los campos nombre, domicilio y población de los compradores no eliminados.
-
+SELECT nombre, domicilio, poblacion FROM compradores;
 
 # 9. Borrar base de datos libreria.
+DROP DATABASE liga_futbol;
